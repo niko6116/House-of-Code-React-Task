24e303d3a15c42d63134c0ff097791521d03d412
Nikolaj Filipsen <niko6116@gmail.com>
2021-06-21 03:29:23 +0200

    Created chat room list with place holders

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
+import ChatRoom from "../components/ChatMessage";
 
 const ChatScreen = props => {
     return <View>
--- src/screens/RoomSelectionScreen.tsx
+++ src/screens/RoomSelectionScreen.tsx
@@ -1,18 +1,32 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { FlatListProperties } from "react-native";
+import { Text, StyleSheet, View, FlatList } from "react-native";
 import ChatRoom from "../components/ChatRoom";
 
 const RoomSelectionScreen = props => {
-    return <View>
-      <Text style={styles.text}>Room Selection Screen</Text>
-      
-    </View>;
+  const rooms = [
+    { name: "Room num 1", description: "First room" },
+    { name: "Room num 2", description: "Second room" },
+    { name: "Room num 3", description: "Third room" }
+  ];
+
+  const Item = ({ item }) => {
+    return <ChatRoom name={item.name} s description={item.description} />;
   };
-  
-  const styles = StyleSheet.create({
-    text: {
-      fontSize: 30
-    }
-  });
+
+  return <View>
+    <Text style={styles.text}>Room Selection</Text>
+    <FlatList
+      data={rooms}
+      renderItem={Item}
+    ></FlatList>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
 
 export default RoomSelectionScreen;
