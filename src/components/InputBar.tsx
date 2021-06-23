f5466a0233f1ef325cddaf2667a268d0fdbb48bf
Nikolaj Filipsen <niko6116@gmail.com>
2021-06-23 07:30:56 +0200

    user input in login screen

---
+++ src/components/InputBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text, StyleSheet, View, TextInput } from "react-native";
+
+const InputBar = (input) => {
+  return <View style={styles.input}>
+    <TextInput
+      style={styles.inputStyle} placeholder="Input"
+      value={input}
+    />
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "#C0C0C0",
+    height: 50,
+    marginHorizontal: 30,
+    borderRadius: 5
+  },
+  inputStyle: {
+    fontSize: 15
+  }
+});
+
+export default InputBar
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 import Authentication from "../utility/Authentication";
+import InputBar from "../components/InputBar";
 
 const LoginScreen = props => {
+  const username = "";
+  const password = "";
+
   return <View>
-    <Text style={styles.text}>Login Screen</Text>
+    <Text style={styles.mainText}>Login with Google</Text>
+    <View style={styles.login}>
+      <Text style={styles.secondText}>Username</Text>
+      <InputBar>input={username}</InputBar>
+      <Text style={styles.secondText}>Password</Text>
+      <InputBar>input={password}</InputBar>
+    </View>
     <Button
-      title="Go to chat rooms"
-      onPress={() => props.navigation.navigate("RoomSelection")}
+      title="Login"
+      onPress={() => loginButtonPressed(props, username, password)}
     />
   </View>;
 };
 
+function loginButtonPressed(props: any, username: string, password: string) {
+
+  props.navigation.navigate("RoomSelection")
+}
+
 const styles = StyleSheet.create({
-  text: {
+  mainText: {
     fontSize: 30
+  },
+  secondText: {
+    fontSize: 20
+  },
+  login: {
+    marginVertical: 40
   }
 });
 
